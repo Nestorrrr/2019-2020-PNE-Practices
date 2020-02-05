844a258cb3830ea8d460b719f9e4c189c3a356a5
Nestorrrr <2001nes@gmail.com>
2020-02-05 18:04:40 +0100

    This is my first change

--- Session_02/Exercise_3_Fibosum.py
+++ Session_02/Exercise_3_Fibosum.py
@@ -1,24 +1,60 @@
-def fibonacci():
+def sumfibonacci():
     lista = []
+    total = 0
     n = 0
     i = 1
     variable = 0
     variable_2 = 1
-    while i < 5:
+    while i <= 6:
         if i == 1:
             i += 1
             lista.append(n)
+            total += n
         elif i == 2:
             i += 1
             n += 1
             lista.append(n)
+            total += n
         else:
             n = lista[variable] + lista[variable_2]
             variable += 1
             variable_2 += 1
             i += 1
             lista.append(n)
-            return lista
+            total += n
+    print('Sum of the First 5 terms of the Fibonacci series: ', total)
 
 
-fibonacci()
+def sumfibonacci_2():
+    lista = []
+    total = 0
+    n = 0
+    i = 1
+    variable = 0
+    variable_2 = 1
+    while i <= 11:
+        if i == 1:
+            i += 1
+            lista.append(n)
+            total += n
+        elif i == 2:
+            i += 1
+            n += 1
+            lista.append(n)
+            total += n
+        else:
+            n = lista[variable] + lista[variable_2]
+            variable += 1
+            variable_2 += 1
+            i += 1
+            lista.append(n)
+            total += n
+    print('Sum of the First 5 terms of the Fibonacci series: ', total)
+
+
+sumfibonacci()
+sumfibonacci_2()
+
+
+
+
