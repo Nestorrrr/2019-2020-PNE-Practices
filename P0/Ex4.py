0cff0cf00766e0760da873f7f356191a2425904a
Nestorrrr <2001nes@gmail.com>
2020-02-25 16:45:40 +0100

    This is my first change

---
+++ P0/Ex4.py
@@ -0,0 +1,14 @@
+from Seq0 import seq_count_base
+
+can_continue = False
+while not can_continue:
+    user_input = input('Type here a valid file: ')
+    FILENAME = user_input + '.txt'
+    if user_input == 'stop':
+        can_continue = True
+    else:
+        FOLDER = "../Session_04/"
+
+        DNA_file = FOLDER + FILENAME
+
+        seq_count_base(DNA_file)
--- P0/Seq0.py
+++ P0/Seq0.py
@@ -16,6 +16,11 @@ def seq_len(filename):
     seq = "".join(file_contents)
     return len(seq)
 
-def seq_count_base():
+def seq_count_base(filename):
     file_contents = pathlib.Path(filename).read_text().split("\n")[1:]
     seq = "".join(file_contents)
+    print('A: ',seq.count('A'))
+    print('C: ',seq.count('C'))
+    print('G: ',seq.count('G'))
+    print('T: ',seq.count('T'))
+
