987f1c73a4fe17113d6d3ee4ffe8b52cd89e645a
Nestorrrr <2001nes@gmail.com>
2020-03-03 16:43:04 +0100

    This is my first change

---
+++ Session_06/test-01.py
@@ -0,0 +1,9 @@
+from Seq0 import *
+
+seq1 = "ATTCCCGGGG"
+
+print('Seq: ',seq1)
+print('Rev: ',seq_reverse('text.txt',seq1))
+print('Comp: ', seq_complement('text.txt',seq1))
+print('Length: ',seq_len('text.txt'))
+seq_count_base('text.txt')
