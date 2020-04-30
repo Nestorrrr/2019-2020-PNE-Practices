b9b7ab06bb15458156aaad211bce8f48cf1c73cf
Nestorrrr <n.estevez.2019@alumnos.urjc.es>
2020-04-30 16:57:48 +0200

    New files added

--- Session_06/Ex2.py
+++ Session_06/Ex2.py
@@ -1,14 +1,27 @@
 class Seq:
 
-    def __init__(self,strbases):
-
+    def __init__(self, strbases):
+        bases = ['A', 'C', 'G', 'T']
+        for element in strbases:
+            if element not in bases:
+                print('ERROR!!')
+                self.strbases = 'ERROR'
+                return
         self.strbases = strbases
-        print('New sequenced created!!')
+        print('New sequence created!')
 
     def __str__(self):
-
         return self.strbases
 
     def len(self):
-
         return len(self.strbases)
+
+
+def print_seqs(seqs):
+    for seq in seqs:
+        print(f'Sequence {seqs.index(seq)}: (Length: {seq.len()})  {seq}')
+
+
+seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
+
+print_seqs(seq_list)
