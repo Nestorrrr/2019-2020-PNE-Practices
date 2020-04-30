413e80f70c214400924566fb9e2f5667505c779b
Nestorrrr <n.estevez.2019@alumnos.urjc.es>
2020-04-30 15:43:15 +0200

    New files added

--- Session_06/Inheritance.py
+++ Session_06/Inheritance.py
@@ -20,9 +20,9 @@ class Seq:
 
 class Gene(Seq):
     """This class is derived from the Seq Class
-           All the objects of class Gene will inherite
-           the methods from the Seq class
-        """
+       All the objects of class Gene will inherite
+       the methods from the Seq class
+    """
     def __init__(self, strbases, name=""):
 
         # -- Call first the Seq initilizer and then the
@@ -31,14 +31,14 @@ class Gene(Seq):
         self.name = name
         print("New gene created")
 
+    def __str__(self):
+        """Print the Gene name along with the sequence"""
+        return self.name + "-" + self.strbases
 
-
-# --Main the main program
-s1 = Seq('AGTACACTGGT')
-g = Gene('CGTAAC')
+# --- Main program
+s1 = Seq("AGTACACTGGT")
+g = Gene("CGTAAC",'FRAT1')
 
 # -- Printing the objects
-print(f'Sequence 1: {s1}')
-print(f'Length: {s1.len()}')
-print(f'Gene: {g}')
-print(f'Length: {g.len()}')
+print(f"Sequence 1: {s1}")
+print(f"Gene: {g}")
