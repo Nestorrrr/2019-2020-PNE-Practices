322b9121c5bc7becff6650f5bd2693c0136941bf
Nestorrrr <n.estevez.2019@alumnos.urjc.es>
2020-05-06 18:06:15 +0200

    New files added

---
+++ P1/Ex2.py
@@ -0,0 +1,9 @@
+from Seq1 import Seq
+
+print('-----| Practice 1, Exercise 2 |------')
+
+sequence1 = Seq()
+sequence2 = Seq("TATAC")
+
+print(f"Sequence 1: {sequence1}")
+print(f"Sequence 2: {sequence2}")
--- P1/Seq1.py
+++ P1/Seq1.py
@@ -1,20 +1,44 @@
+from pathlib import Path
+
+
 class Seq:
 
-    def __init__(self, strbases):
-        bases = ['A', 'C', 'G', 'T']
-        for element in strbases:
-            if element not in bases:
-                print('ERROR!!')
-                self.strbases = 'ERROR'
-                return
+    NULL = "NULL"
+    ERROR = "ERROR"
+
+    def __init__(self, strbases=NULL):
+        if strbases == self.NULL:
+            self.strbases = self.NULL
+            print("NULL Seq created")
+            return
+
+        if not self.valid_str(strbases):
+            self.strbases = self.ERROR
+            print("INVALID Seq!")
+            return
+
         self.strbases = strbases
         print('New sequence created')
 
     def __str__(self):
         return self.strbases
 
+    @staticmethod
+    def valid_str(strbases):
+
+        valid_bases = ['A', 'C', 'T', 'G']
+
+        for element in strbases:
+            if element not in valid_bases:
+                return False
+
+        return True
+
     def len(self):
-        return len(self.strbases)
+        if self.strbases in [self.NULL, self.ERROR]:
+            return 0
+        else:
+            return len(self.strbases)
 
 
 def print_seqs(seqs):
