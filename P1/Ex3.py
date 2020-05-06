6a9e9923a6e69c90d28f4691d37a40491f8a05c0
Nestorrrr <n.estevez.2019@alumnos.urjc.es>
2020-05-06 18:24:17 +0200

    New files added

---
+++ P1/Ex3.py
@@ -0,0 +1,12 @@
+from Seq1 import Seq
+
+print('-----| Practice 1, Exercise 3 |------')
+
+sequence1 = Seq()
+sequence2 = Seq("ACTGA")
+sequence3 = Seq("Invalid sequence")
+
+print(f"Sequence 1: {sequence1}")
+print(f"Sequence 2: {sequence2}")
+print(f"Sequence 3: {sequence3}")
+
---
+++ P1/Ex4.py
@@ -0,0 +1,12 @@
+from Seq1 import Seq
+
+print('-----| Practice 1, Exercise 3 |------')
+
+sequence1 = Seq()
+sequence2 = Seq("ACTGA")
+sequence3 = Seq("Invalid sequence")
+
+print(f"Sequence 1:  (Lenght: {sequence1.len()})  {sequence1}")
+print(f"Sequence 2:  (Lenght: {sequence2.len()})  {sequence2}")
+print(f"Sequence 3:  (Lenght: {sequence3.len()})  {sequence3}")
+
