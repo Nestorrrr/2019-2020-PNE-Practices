80e9a0a3a4c8002864ed2a6139b4c21d94aa57c5
Nestorrrr <n.estevez.2019@alumnos.urjc.es>
2020-05-06 18:26:32 +0200

    New files added

--- P1/Ex4.py
+++ P1/Ex4.py
@@ -9,4 +9,3 @@ sequence3 = Seq("Invalid sequence")
 print(f"Sequence 1:  (Lenght: {sequence1.len()})  {sequence1}")
 print(f"Sequence 2:  (Lenght: {sequence2.len()})  {sequence2}")
 print(f"Sequence 3:  (Lenght: {sequence3.len()})  {sequence3}")
-
