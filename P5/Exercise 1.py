a52829223a6a6f8eb520eea1d62dc57f442d08d9
Nestorrrr <n.estevez.2019@alumnos.urjc.es>
2020-05-23 21:42:46 +0200

    New!

--- P5/Exercise 1.py
+++ P5/Exercise 1.py
@@ -9,24 +9,68 @@ socketserver.TCPServer.allow_reuse_address = True
 
 
 def read_file(filename):
-    file_contents = pathlib.Path(filename).read_text().split("\n")[1:]
+    file_contents = pathlib.Path(filename).read_text().split('\n')[1:]
     body = "".join(file_contents)
     return body
+
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         termcolor.cprint(self.requestline, 'green')
 
-        # Message to send back to the client
         FOLDER = "../P5/"
-        if self.path == '/' or self.path =='/index.html':
-            file = "index.html"
+        print(self.path)
+        if self.path == '/' or self.path == '/index.html':
+            file = 'index.html'
+
+        elif self.path == '/info/A':
+            file = 'info/A.html'
+
+        elif '/info/C' in self.path:
+            file = 'info/C.html'
+
+        elif '/info/G' in self.path:
+            file = 'info/G.html'
+
+        elif '/info/T' in self.path:
+            file = 'info/T.html'
 
         else:
-            file = self.path
+            file = 'Error.html'
 
         try:
             contents = read_file(FOLDER + file)
             self.send_response(200)
-        except
+
+        except FileNotFoundError:
+            contents = read_file(FOLDER + 'Error.html')
+            self.send_response(404)
+
+        self.send_header('Content/Type: ', 'text/html')
+        self.send_header('Content/Length: ', len(contents.encode()))
+
+        self.end_headers()
+        self.wfile.write(contents.encode())  # Send the response message
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
